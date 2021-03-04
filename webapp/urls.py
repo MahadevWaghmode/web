a8f379d5bb7ed7e558b45d946b46cc573ad26a68
MahadevWaghmode <45910438+MahadevWaghmode@users.noreply.github.com>
2021-03-04 21:31:44 +0530

    slidebar
    slidebar added

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home',views.home, name='home'),
     path('search', views.search, name='search'),
+    path('movie',views.movie, name='movie'),
+    path('searchm',views.searchm, name='searchm'),
+    path('game',views.game,name='game'),
+    path('profile',views.profile,name='profile'),
+    path('about',views.about,name='about'),
+    path('contact',views.about,name='contact'),
+    path('software',views.software,name="software"),
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -7,6 +7,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def home(request):
     return render(request,'index.html') 
+
+
+def movie(request):
+    return render(request, 'movie.html')
+
+def searchm(request):
+    if request.method=='POST':
+        srch = request.POST['box']
+
+        if srch:
+            match = movies.objects.filter(Q(name__icontains=srch) 
+                                        
+                            )
+            if match:
+                return render(request,'result.html',{'result':match,'srch':srch})
+ 
+    return render(request,'index.html')
 def search(request):
     if request.method=='POST':
         srch = request.POST['box']
@@ -19,3 +36,19 @@ def search(request):
                 return render(request,'result.html',{'result':match,'srch':srch})
  
     return render(request,'index.html')
+
+def game(request):
+    return render(request, 'contruction.html')
+
+def profile(request):
+    return render(request, 'contruction.html')
+    
+def about(request):
+    return render(request, 'contruction.html')
+    
+def contact(request):
+    return render(request, 'contruction.html')
+    
+def software(request):
+    return render(request, 'contruction.html')
+
